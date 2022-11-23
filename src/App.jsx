eac16db9cb6035187f3a29bd088f4b7749fc5f68
Youssef Elaissaoui <yelaissa@c1r3p8.1337.ma>
2022-11-23 17:48:38 +0100

    Weather app

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import SearchBar from "./components/SearchBar";
 import WeatherCard from "./components/WeatherCard";
 
@@ -6,25 +7,27 @@ export default function App() {
   const [focused, setFocused] = useState(false);
 
   function onFocus() {
-    setFocused(true)
+    setFocused(true);
   }
   function onBlur() {
-    setFocused(false)
+    setFocused(false);
   }
   return (
     <div className="container">
       <div className="relative">
-        <h1 className="font-bold text-4xl py-4">Weather</h1>
-        <SearchBar onFocuss={onFocus} onBlurr={onBlur}/>
-          <div className={!focused ? `animate-pulseslow` : ""}>
-            <div className="absolute -bottom-4 left-20 w-60 h-60 bg-teal-400 rounded-full mix-blend-screen filter blur-3xl opacity-20 -z-10 animate-blob"></div>
-            <div className="absolute top-0 -left-4 w-72 h-72 bg-purple-600 rounded-full mix-blend-screen filter blur-3xl opacity-20 -z-10 animate-blob "></div>
-            <div className="absolute top-0 -right-4 w-72 h-72 bg-yellow-400 rounded-full mix-blend-screen filter blur-3xl opacity-20 -z-10 animate-blob"></div>
-          </div>
+        <h1 className="relative z-50 font-bold text-4xl py-4">Weather</h1>
+        <SearchBar onFocuss={onFocus} onBlurr={onBlur} />
+        <div className={!focused ? "animate-pulseslow" : ""}>
+          <div className="absolute -bottom-4 left-20 w-60 h-60 bg-teal-400 rounded-full mix-blend-screen filter blur-3xl opacity-20 -z-10 animate-blob"></div>
+          <div className="absolute top-0 -left-4 w-72 h-72 bg-purple-600 rounded-full mix-blend-screen filter blur-3xl opacity-20 -z-10 animate-blob "></div>
+          <div className="absolute top-0 -right-4 w-72 h-72 bg-yellow-400 rounded-full mix-blend-screen filter blur-3xl opacity-20 -z-10 animate-blob"></div>
+        </div>
       </div>
       <div className="weathers my-8 relative">
+        <p className="font-medium my-4 mx-2 text-gray-200 opacity-50">Your current location</p>
         <WeatherCard />
-        <WeatherCard />
+        <hr className="w-80 mx-auto opacity-10"/>
+        <br />
         <WeatherCard />
       </div>
     </div>
--- src/components/WeatherCard.jsx
+++ src/components/WeatherCard.jsx
@@ -1,38 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
 import Tilty from "react-tilty";
 export default function WeatherCard() {
+  const [shadow, setShadow] = useState(false);
+
+  function showShadow() {
+    setShadow(true);
+  }
+  function hideShadow() {
+    setShadow(false);
+  }
   return (
     <Tilty max={10}>
-        <div className="mb-5 cursor-pointer weather-card py-3 px-5 w-full rounded-3xl relative overflow-hidden">
-          <div className="layer"></div>
-          <img
-            className="absolute inset-0 rounded -translate-y-4 -z-10"
-            src="https://images.unsplash.com/photo-1517685352821-92cf88aee5a5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2148&q=80"
-            alt="cloudy"
-          />
-          <div className="relative">
-            <div className="flex justify-between mb-5">
-              <div>
-                <h1 className="text-3xl font-semibold text-shadow">Tetouan</h1>
-                <p className="font-bold opacity-60">
-                  {new Date().getHours()}:
-                  {new Date().getMinutes() <= 9
-                    ? `0${new Date().getMinutes()}`
-                    : new Date().getMinutes()}
-                </p>
-              </div>
-              <h1 className="text-5xl font-medium">20˚</h1>
+      <div
+        className={`duration-300 absolute bg-gray-200 -z-10 inset-8 filter blur-3xl ${
+          !shadow ? "opacity-0" : "opacity-60"
+        }`}
+      ></div>
+      <div
+        className="mb-5 cursor-pointer weather-card py-3 px-5 w-full rounded-3xl relative overflow-hidden"
+        onMouseOver={() => showShadow()}
+        onMouseLeave={() => hideShadow()}
+      >
+        <div className="layer"></div>
+        <img
+          className="absolute inset-0 rounded -translate-y-4 -z-10"
+          src="https://images.unsplash.com/photo-1517685352821-92cf88aee5a5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2148&q=80"
+          alt="cloudy"
+        />
+        <div className="relative">
+          <div className="flex justify-between mb-5">
+            <div>
+              <h1 className="text-3xl font-semibold text-shadow">Tetouan</h1>
+              <p className="font-bold opacity-60">
+                {new Date().getHours()}:
+                {new Date().getMinutes() <= 9
+                  ? `0${new Date().getMinutes()}`
+                  : new Date().getMinutes()}
+              </p>
             </div>
+            <h1 className="text-5xl font-medium">20˚</h1>
+          </div>
 
-            <div className="font-medium flex justify-between items-center">
-              <p className="opacity-60">Partly cloudy</p>
-              <div className="flex gap-2 opacity-80 font-medium text-shadow">
-                <p>H:21˚</p>
-                <p>L:16˚</p>
-              </div>
+          <div className="font-medium flex justify-between items-center">
+            <p className="opacity-60">Partly cloudy</p>
+            <div className="flex gap-2 opacity-80 font-medium text-shadow">
+              <p>H:21˚</p>
+              <p>L:16˚</p>
             </div>
           </div>
         </div>
+      </div>
     </Tilty>
   );
 }

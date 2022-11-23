ffb8aa7819977dacfdc4671747cb1f91dbffd079
Youssef Elaissaoui <yelaissa@c1r3p8.1337.ma>
2022-11-23 20:11:45 +0100

    Weather api

--- src/api/weatherApi.js
+++ src/api/weatherApi.js
@@ -1,38 +1,53 @@
-// import axios from "axios";
-
-// const API_KEY = "1421fdd53f1f23530c9e6d68843277c1";
-// const BASE_URL = "https://api.openweathermap.org/data/2.5/";
-
-// const getData = (infoType, searchParams) => {
-//   const url = new URL(BASE_URL + infoType);
-//   url.search = new URLSearchParams({ ...searchParams, appid: API_KEY });
-
-//   return axios.get(url)
-//     .then((res) => res.data);
-// };
-
-// const getWeatherData = (searchParams) => {
-// 	const currentWeather = await getData('weather')
-// }
-
 import axios from "axios";
 
-const getWeather = () => {
-  const options = {
-    method: "GET",
-    url: "https://forecast9.p.rapidapi.com/rapidapi/forecast/Berlin/summary/",
-    headers: {
-      "X-RapidAPI-Key": "4aa9b73f29msh054f16f33f85b5dp1f0a5djsn97399ea7a891",
-      "X-RapidAPI-Host": "forecast9.p.rapidapi.com",
-    },
-  };
+const API_KEY = "1421fdd53f1f23530c9e6d68843277c1";
+const BASE_URL = "https://api.openweathermap.org/data/2.5/";
 
-  axios
-    .request(options)
-    .then(function (response) {
-      console.log(response.data);
-    })
-    .catch(function (error) {
-      console.error(error);
-    });
+const getData = (infoType, searchParams) => {
+  const url = new URL(BASE_URL + infoType);
+  url.search = new URLSearchParams({ ...searchParams, appid: API_KEY });
+
+  return axios.get(url).then((res) => res.data);
 };
+
+const formatCurrentWeather = (data) => {
+  const {
+    coord: { lat, lon },
+    main: { temp, feels_like, temp_min, temp_max, humidity },
+    name,
+    dt,
+    sys: { country, sunrise, sunset },
+    weather,
+    wind: { speed },
+  } = data;
+
+  const { main: details, description, icon } = weather[0];
+
+  return {
+    lat,
+    lon,
+    temp,
+    feels_like,
+    temp_min,
+    temp_max,
+    humidity,
+    name,
+    dt,
+    country,
+    sunrise,
+    sunset,
+    weather,
+    speed,
+    details,
+    description,
+    icon,
+  };
+};
+
+const getWeatherData = (searchParams) => {
+	const currentWeather = await getData('weather', searchParams).then(formatCurrentWeather)
+
+  return currentWeather
+}
+
+export default getWeatherData

95aa2f689493aacb3e26e584666a5bcfeaaf544f
YusufLisawi <yusufisawi@gmail.com>
2022-12-09 11:25:24 +0100

    styling

--- src/components/Forcast.jsx
+++ src/components/Forcast.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Forcast({ weather }) {
 	return (
 		<div className="forecast grid place-content-center mb-4">
-			<div className="flex gap-8 text-center font-bold text-xl bg-gray-800/10 backdrop-blur-sm py-3 px-4 rounded-3xl">
+			<div className="flex gap-8 text-center font-bold text-xl border border-gray-400/10 bg-gray-800/10 backdrop-blur-sm py-3 px-4 rounded-3xl">
 				{weather.map((d) => (
 					<div key={weather.lat}>
 						<h2 className="font-semibold text-lg">{d.title}</h2>
--- src/components/SunTime.jsx
+++ src/components/SunTime.jsx
@@ -5,14 +5,14 @@ import { formatToLocalTime } from "../api/weatherApi";
 export default function SunTime({ sunrise, sunset, timezone }) {
 	return (
 		<div className="flex justify-center mb-4 gap-4">
-			<div className="text-xl bg-gray-800/10 backdrop-blur-sm py-3 px-4 rounded-3xl">
+			<div className="text-xl bg-gray-800/10 border b-2 border-gray-400/10 backdrop-blur-sm py-3 px-4 rounded-3xl">
 				<BsFillSunriseFill size={40} />
 				<p>Sunrise</p>
 				<p className="font-bold">
 					{formatToLocalTime(sunrise, timezone, "hh:mm a")}
 				</p>
 			</div>
-			<div className="text-xl bg-gray-800/10 backdrop-blur-sm py-3 px-4 rounded-3xl">
+			<div className="text-xl bg-gray-800/10 border b-2 border-gray-400/10 backdrop-blur-sm py-3 px-4 rounded-3xl">
 				<BsFillSunsetFill size={40} />
 				<p>Sunset</p>
 				<p className="font-bold">
